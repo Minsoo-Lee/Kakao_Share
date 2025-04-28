9e84d83e84b5909a1e4bb1bac6e8f91174eff162
Minsoo Lee <minsoo.james1101@gmail.com>
2025-04-28 23:35:56 +0900

    message template

--- web/server.py
+++ web/server.py
@@ -23,48 +23,48 @@ def run_flask(on_done_callback):
     log.append_log("서버를 시작합니다.")
     app.run(debug=True, port=9005, use_reloader=False)
 
-@app.route('/')
-def share():
-    summaries = cr.news_list
-    contents = []
-    default_image = 'http://k.kakaocdn.net/dn/bDPMIb/btqgeoTRQvd/49BuF1gNo6UXkdbKecx600/kakaolink40_original.png'
-    default_link = 'https://developers.kakao.com'
-
-
-    print(json.dumps(summaries, indent=4, ensure_ascii=False))
-
-    for i in range(3):
-        contents.append({
-            'title': summaries[i]["title"],
-            'description': summaries[i]["description"], # 필요에 따라 변경
-            'imageUrl': summaries[i]["img"] if summaries[i]["img"] else default_image,
-            'link': {
-                'mobileWebUrl': summaries[i]["url"],
-                'webUrl': summaries[i]["url"],
-            },
-        })
-        print(contents[i])
-        print("=" * 30)
-
-
-    # summaries 리스트의 길이가 3보다 작으면 기본 contents를 채워줍니다.
-    while len(contents) < 3:
-        contents.append({
-            'title': '기사 제목 없음',
-            'description': '내용 없음',
-            'imageUrl': default_image,
-            'link': {
-                'mobileWebUrl': default_link,
-                'webUrl': default_link,
-            },
-        })
-
-    return render_template('shared.html', app_key='c03ce9560aa54cba52b9fc2c4db6b3aa', contents=contents)
+# @app.route('/')
+# def share():
+#     summaries = cr.news_list
+#     contents = []
+#     default_image = 'http://k.kakaocdn.net/dn/bDPMIb/btqgeoTRQvd/49BuF1gNo6UXkdbKecx600/kakaolink40_original.png'
+#     default_link = 'https://developers.kakao.com'
+#
+#
+#     print(json.dumps(summaries, indent=4, ensure_ascii=False))
+#
+#     for i in range(3):
+#         contents.append({
+#             'title': summaries[i]["title"],
+#             'description': summaries[i]["description"], # 필요에 따라 변경
+#             'imageUrl': summaries[i]["img"] if summaries[i]["img"] else default_image,
+#             'link': {
+#                 'mobileWebUrl': summaries[i]["url"],
+#                 'webUrl': summaries[i]["url"],
+#             },
+#         })
+#         print(contents[i])
+#         print("=" * 30)
+#
+#
+#     # summaries 리스트의 길이가 3보다 작으면 기본 contents를 채워줍니다.
+#     while len(contents) < 3:
+#         contents.append({
+#             'title': '기사 제목 없음',
+#             'description': '내용 없음',
+#             'imageUrl': default_image,
+#             'link': {
+#                 'mobileWebUrl': default_link,
+#                 'webUrl': default_link,
+#             },
+#         })
+#
+#     return render_template('shared.html', app_key='c03ce9560aa54cba52b9fc2c4db6b3aa', contents=contents)
 
 # @app.route('/')
 # def share():
 #     return render_template('share.html', app_key='c03ce9560aa54cba52b9fc2c4db6b3aa')
 
-# @app.route('/')
-# def share():
-#     return render_template('feed.html', app_key='c03ce9560aa54cba52b9fc2c4db6b3aa')
+@app.route('/')
+def share():
+    return render_template('feed.html', app_key='c03ce9560aa54cba52b9fc2c4db6b3aa')
